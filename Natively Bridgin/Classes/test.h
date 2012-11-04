6126edcd12449458a6b8a65c0e6ccf4e5803fed3
Jomana Sherif <techjomana@gmail.com>
2012-11-03 21:14:29 -0700

    Example Native Briding for iOS

---
+++ Natively Bridgin/Classes/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  NativeBridge-non-buggy
+//
+//  Created by Sherif on 11/3/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ Natively Bridgin/Classes/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  NativeBridge-non-buggy
+//
+//  Created by Sherif on 11/3/12.
+//
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
