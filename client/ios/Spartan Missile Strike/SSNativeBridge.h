c06a21feb90838b4150ae364223249c2c1b1dedb
Jomana Sherif <techjomana@gmail.com>
2012-12-22 00:40:05 -0800

    added classes

---
+++ client/ios/Spartan Missile Strike/SSNativeBridge.h
@@ -0,0 +1,13 @@
+//
+//  SSNativeBridge.h
+//  Spartan Missile Strike
+//
+//  Created by Sherif on 12/21/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSNativeBridge : UIView
+
+@end
---
+++ client/ios/Spartan Missile Strike/SSNativeBridge.m
@@ -0,0 +1,31 @@
+//
+//  SSNativeBridge.m
+//  Spartan Missile Strike
+//
+//  Created by Sherif on 12/21/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import "SSNativeBridge.h"
+
+@implementation SSNativeBridge
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
