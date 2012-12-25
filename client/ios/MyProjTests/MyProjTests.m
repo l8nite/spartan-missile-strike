89e27a24099f278859af26c4a5da943e36991852
Jomana Sherif <techjomana@gmail.com>
2012-12-24 21:38:23 -0800

    fixed folders and understood unit tests.

---
+++ client/ios/MyProjTests/MyProjTests.m
@@ -0,0 +1,32 @@
+//
+//  MyProjTests.m
+//  MyProjTests
+//
+//  Created by Sherif on 12/24/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import "MyProjTests.h"
+
+@implementation MyProjTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyProjTests");
+}
+
+@end
---
+++ client/ios/Spartan Missile StrikeTests/SSSplashScreenView.h
@@ -0,0 +1,13 @@
+//
+//  SSSplashScreenView.h
+//  Spartan Missile Strike
+//
+//  Created by Sherif on 12/24/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSSplashScreenView : SenTestCase
+
+@end
