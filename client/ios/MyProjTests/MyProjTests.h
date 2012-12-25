b7e98ce6fbafef4e293f8d08b0e1f71c4a1893e5
Jomana Sherif <techjomana@gmail.com>
2012-12-24 21:38:23 -0800

    fixed folders and understood unit tests.

---
+++ client/ios/MyProjTests/MyProjTests.h
@@ -0,0 +1,13 @@
+//
+//  MyProjTests.h
+//  MyProjTests
+//
+//  Created by Sherif on 12/24/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MyProjTests : SenTestCase
+
+@end
---
+++ client/ios/Spartan Missile StrikeTests/SSSplashScreenView.m
@@ -0,0 +1,13 @@
+//
+//  SSSplashScreenView.m
+//  Spartan Missile Strike
+//
+//  Created by Sherif on 12/24/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import "SSSplashScreenView.h"
+
+@implementation SSSplashScreenView
+
+@end
