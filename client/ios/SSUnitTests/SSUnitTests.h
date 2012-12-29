c895739880f66613885c5df1a6f0f2a484976da7
Jomana Sherif <techjomana@gmail.com>
2012-12-29 12:06:01 -0800

    Created only one Unit Test case file, this is where all the tests will go "SSUnitTests"

---
+++ client/ios/SSUnitTests/SSUnitTests.h
@@ -0,0 +1,13 @@
+//
+//  SSUnitTests.h
+//  SSUnitTests
+//
+//  Created by Sherif on 12/29/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSUnitTests : SenTestCase
+
+@end
---
+++ client/ios/SSUnitTests/SSUnitTests.m
@@ -0,0 +1,32 @@
+//
+//  SSUnitTests.m
+//  SSUnitTests
+//
+//  Created by Sherif on 12/29/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import "SSUnitTests.h"
+
+@implementation SSUnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SSUnitTests");
+}
+
+@end
