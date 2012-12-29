038cdf9321a3fa78a9d7a25b46418222fe1115f6
Jomana Sherif <techjomana@gmail.com>
2012-12-29 12:06:01 -0800

    Created only one Unit Test case file, this is where all the tests will go "SSUnitTests"

---
+++ client/ios/SSFiring/SSFiring.m
@@ -0,0 +1,32 @@
+//
+//  SSFiring.m
+//  SSFiring
+//
+//  Created by Sherif on 12/29/12.
+//  Copyright (c) 2012 Jomana Sherif. All rights reserved.
+//
+
+#import "SSFiring.h"
+
+@implementation SSFiring
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SSFiring");
+}
+
+@end
