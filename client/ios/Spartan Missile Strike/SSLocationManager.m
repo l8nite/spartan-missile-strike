2c75b2e4680da4e0c3e197a30dbb0f3de8b7e6b5
Jomana Sherif <techjomana@gmail.com>
2013-01-01 15:10:39 -0800

    Added content to SSLocationManager.

--- client/ios/Spartan Missile Strike/SSLocationManager.m
+++ client/ios/Spartan Missile Strike/SSLocationManager.m
@@ -9,5 +9,30 @@
 #import "SSLocationManager.h"
 
 @implementation SSLocationManager
+@synthesize longitude, latitude, horizontalAccuracy, verticalAccuracy,altitude,distance;
+@synthesize locationManager, startLocation;
+
+
+
+
+- (void)viewDidLoad {
+    self.locationManager = [[CLLocationManager alloc] init];
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    startLocation = nil;
+    [super viewDidLoad];
+}
+
+
+-(void)resetDistance
+{
+    startLocation = nil;
+}
+
+-(void)locationManager:(CLLocationManager *)manager
+      didFailWithError:(NSError *)error
+{
+}
 
 @end
