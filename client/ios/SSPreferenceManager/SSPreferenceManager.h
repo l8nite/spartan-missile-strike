a8ff734f6e5e5002c188d698fb09f7aa6aef5e2c
Jomana Sherif <techjomana@gmail.com>
2013-01-01 14:43:35 -0800

    Added unit tests classes, they are empty

---
+++ client/ios/SSPreferenceManager/SSPreferenceManager.h
@@ -0,0 +1,13 @@
+//
+//  SSPreferenceManager.h
+//  SSPreferenceManager
+//
+//  Created by Sherif on 1/1/13.
+//  Copyright (c) 2013 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSPreferenceManager : SenTestCase
+
+@end
