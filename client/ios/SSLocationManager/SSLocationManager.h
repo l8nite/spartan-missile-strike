165578e357855e0a62f29c7cf1850b65c8d5ac1e
Jomana Sherif <techjomana@gmail.com>
2013-01-01 14:43:35 -0800

    Added unit tests classes, they are empty

---
+++ client/ios/SSLocationManager/SSLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  SSLocationManager.h
+//  SSLocationManager
+//
+//  Created by Sherif on 1/1/13.
+//  Copyright (c) 2013 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSLocationManager : SenTestCase
+
+@end
---
+++ client/ios/SSSpashScreen/SSSpashScreen.h
@@ -0,0 +1,13 @@
+//
+//  SSSpashScreen.h
+//  SSSpashScreen
+//
+//  Created by Sherif on 1/1/13.
+//  Copyright (c) 2013 Jomana Sherif. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSSpashScreen : SenTestCase
+
+@end
