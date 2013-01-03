1baba827062846caf3c9b446bb64ba25296f97bc
Jomana Sherif <techjomana@gmail.com>
2013-01-03 14:59:41 -0800

    Preference Manager Test skeleton works Horray!

---
+++ client/ios/SpartanMissileStrike/SpartanMissileStrikeTests/SSPreferenceManagerTests.m
@@ -0,0 +1,19 @@
+//
+//  SSPreferenceManagerTests.m
+//  SpartanMissileStrike
+//
+//  Created by Sherif on 1/3/13.
+//  Copyright (c) 2013 Group 2. All rights reserved.
+//
+
+#import "SSPreferenceManagerTests.h"
+
+@implementation SSPreferenceManagerTests
+
+-(void)testPreferences
+{
+    SSPreferenceManager* sp1 = [[SSPreferenceManager alloc] init];
+    STAssertNotNil(sp1,@"SSPreferenceManager initialization test");
+}
+
+@end
