4946ecb1d8d1b0f32908bd22b37755b573609651
Shaun Guth <shaun.guth@gmail.com>
2013-01-03 13:50:56 -0800

    dsadasd

---
+++ client/ios/Test.m
@@ -0,0 +1,41 @@
+//
+//  Test.m
+//  Spartan Missile Strike
+//
+//  Created by Shaun Guth on 1/3/13.
+//  Copyright (c) 2013 Jomana Sherif. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+-(void)init {
+    
+    
+    
+    NSUserDefaults foo;
+    foo =     NSUserDefaults.standardUserDefaults();
+}
+
+public void foo(int a, bool b) {
+    
+}
+
+
+-(void)methodName:parameter1 paramter2Name:parameter2 parameter3Name:parameter3
+
+-(void)setPreference:forKey:
+
+-(void)preferenceForKey
+
+
+getPreference
+setPreference
+
+preference();
+
+setPreference
+preferenceForKey
+
+
