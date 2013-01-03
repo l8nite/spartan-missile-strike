441d1e99c921834e54e6f14093f8fb20b4fedb07
Jomana Sherif <techjomana@gmail.com>
2013-01-03 14:48:02 -0800

    Starting fresh.

---
+++ client/ios/SpartanMissileStrike/SpartanMissileStrikeTests/SpartanMissileStrikeTests.m
@@ -0,0 +1,32 @@
+//
+//  SpartanMissileStrikeTests.m
+//  SpartanMissileStrikeTests
+//
+//  Created by Sherif on 1/3/13.
+//  Copyright (c) 2013 Group 2. All rights reserved.
+//
+
+#import "SpartanMissileStrikeTests.h"
+
+@implementation SpartanMissileStrikeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SpartanMissileStrikeTests");
+}
+
+@end
