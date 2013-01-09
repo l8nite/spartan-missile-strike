6a9d29f4b3f14eb457eba4cde6200bfbe0524822
Jomana Sherif <techjomana@gmail.com>
2013-01-09 13:54:57 -0800

    Fixed project structure.

---
+++ client/ios/SpartanMissileStrike/SSMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSMainViewController.m
+//  SpartanMissileStrike
+//
+//  Created by Sherif on 1/9/13.
+//  Copyright (c) 2013 Group 2. All rights reserved.
+//
+
+#import "SSMainViewController.h"
+
+@interface SSMainViewController ()
+
+@end
+
+@implementation SSMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
