b90bc9f4d9658bb9a76002baf1edddea0a507c55
Jomana Sherif <techjomana@gmail.com>
2013-01-11 10:36:00 -0800

    included the word tests after each test class. Basically added the test classes again the correct way.

---
+++ client/ios/SpartanMissileStrikeTests/SSFacebookAuthTests.h
@@ -0,0 +1,13 @@
+//
+//  SSFacebookAuthTests.h
+//  SpartanMissileStrike
+//
+//  Created by Sherif on 1/11/13.
+//  Copyright (c) 2013 Group 2. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSFacebookAuthTests : SenTestCase
+
+@end
