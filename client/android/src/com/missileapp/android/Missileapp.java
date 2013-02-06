bbe6056de8daa7603a5d6f19cbd18c0e4ff8c0a6
srkarra <saikarra@gmail.com>
2013-02-05 21:05:35 -0800

    Refactored name to MissileApp instead of Missileapp

--- client/android/src/com/missileapp/android/Missileapp.java
+++ client/android/src/com/missileapp/android/Missileapp.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Missileapp extends Activity {
+public class MissileApp extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
