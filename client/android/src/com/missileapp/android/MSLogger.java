dff106c6e3ce5da89796a529b897879b2fd45272
srkarra <saikarra@gmail.com>
2013-02-09 10:44:18 -0800

    Documented Logging

--- client/android/src/com/missileapp/android/MSLogger.java
+++ client/android/src/com/missileapp/android/MSLogger.java
@@ -1,30 +1,41 @@
 package com.missileapp.android;
 
 import java.util.Date;
-
 import android.util.Log;
-
 import com.missileapp.android.BuildConfig;
 
 public class MSLogger {
+	/**
+	 * Logging function, See {@link Log}
+	 * @param tag - Class that needs to log 
+	 * @param LogType - Type of Log, Look at {@link Log#ASSERT}}
+	 * @param msg - Message to print. Date and Time is automatically added {@link Date#toString()}
+	 * @param tr - Throwable to print {@link Throwable}
+	 */
 	public static void log(String tag, int LogType, String msg, Throwable tr) {
-		msg = (msg != null) ? (new Date() +  ": " + msg) : (new Date() +  ": No Msg!"); 
+		// Add Timestamp to msg
+		msg = (msg != null) ? (new Date() +  ": " + msg) : (new Date() +  ": No Msg!");
+		
+		// Log only in debug mode
 		if(BuildConfig.DEBUG) {
 			switch (LogType) {
+				//DEBUG
 				case Log.DEBUG:
 					if(tr != null)
 						Log.d(tag, msg, tr);
 					else
 						Log.d(tag, msg);
 					break;
-					
+				
+				//ERROR
 				case Log.ERROR:
 					if(tr != null)
 						Log.e(tag, msg, tr);
 					else
 						Log.e(tag, msg);
 					break;
-					
+				
+				//Info
 				case Log.INFO:
 					if(tr != null)
 						Log.i(tag, msg, tr);
@@ -32,6 +43,7 @@ public class MSLogger {
 						Log.i(tag, msg);
 					break;
 				
+				//VERBOSE
 				case Log.VERBOSE:
 					if(tr != null)
 						Log.v(tag, msg, tr);
@@ -39,6 +51,7 @@ public class MSLogger {
 						Log.v(tag, msg);
 					break;
 				
+				//WARN
 				case Log.WARN:
 					if(tr != null)
 						Log.w(tag, msg, tr);
@@ -46,6 +59,7 @@ public class MSLogger {
 						Log.w(tag, msg);
 					break;
 	
+				//WTF Happened?! See Log.wtf()
 				default:
 					if(tr != null)
 						Log.wtf(tag, msg, tr);
