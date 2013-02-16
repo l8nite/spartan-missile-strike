73779667c464760e77dae4c23bedfc625b7a0fbf
Shaun Guth <shaun.guth@gmail.com>
2013-02-16 13:32:22 +0400

    configure test restify client before tests

--- service/test/99-facebook-test-cleanup.js
+++
@@ -1,9 +0,0 @@
-var should = require("should");
-var fbgraph = require('fbgraph');
-
-var fbconf = require("../lib/facebook-config.js");
-
-after(function(done) {
-    console.log("TODO: delete facebook test user");
-    done();
-});
--- service/test/sessions.js
+++ service/test/sessions.js
@@ -1,24 +1,8 @@
 var should = require('should');
-var restify = require('restify');
-var fbgraph = require('fbgraph');
-var fs = require('fs');
-
-var client = restify.createJsonClient({
-    url: 'https://localhost:8433',
-    version: '*'});
-
-var fbconf = require("../lib/facebook-config.js");
-
-before(function(done) {
-    console.log("This executed A");
-    done();
-});
+var client = require('./lib/service-test.js').client;
+//var fbUser = require('./lib/facebook-test.js').user;
 
 describe('/sessions', function() {
-    before(function(done) {
-        console.log("This executed B");
-        done();
-    });
     it('should return a 201 when facebook_access_token param is sent', function (done) {
         client.post('/sessions', { facebook_access_token: "abcdefg" }, function(err, req, res, obj) {
             res.statusCode.should.equal(201);
