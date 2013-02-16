5d92f29904247d67ad00a45759d29923135976f0
Shaun Guth <shaun.guth@gmail.com>
2013-02-16 11:40:55 +0400

    ensure database is connected before starting server

--- service/lib/database.js
+++ service/lib/database.js
@@ -1,15 +1,32 @@
-// set up our database connection
-var redis = require('redis').createClient('6379', 'localhost');
+var redis = require('redis');
+var client;
 
-redis.on('error', function (err) {
-    console.log("Redis Error: " + err);
-    process.exit(1);
-});
+var connect = function(done) {
+    if (client !== undefined) {
+        done(undefined, client);
+    }
 
-redis.on('connect', function () {
-    console.log("Redis Connected");
-});
+    console.log("Connecting to database...");
+    client = redis.createClient('6379', 'localhost');
 
-redis.select(1);
+    client.on('error', function (err) {
+        console.log("Redis Error: " + err);
+        process.exit(1);
+    });
 
-exports.redis = redis;
+    client.on('connect', function () {
+        console.log("Redis Connected");
+        client.select(1, function (err) {
+            if (!err) {
+                done(undefined, client);
+            }
+            else {
+                console.log("Could not select(1): " + err);
+                process.exit(1);
+            }
+        });
+    });
+};
+
+module.exports.connect = connect;
+module.exports.redisClient = client;
