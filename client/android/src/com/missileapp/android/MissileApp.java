dc2acb21817e62dc9baa38f9c77b490238d63e02
srkarra <saikarra@gmail.com>
2013-02-19 15:39:42 -0800

    Added Android launch callback functions and camera surface callback functions

--- client/android/src/com/missileapp/android/MissileApp.java
+++ client/android/src/com/missileapp/android/MissileApp.java
@@ -2,13 +2,16 @@ package com.missileapp.android;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.webkit.WebView;
 import android.app.Activity;
 import android.content.SharedPreferences;
 
-public class MissileApp extends Activity {
+public class MissileApp extends Activity implements SurfaceHolder.Callback {
     //TODO [MARKER] REMOVE WAKELOCK FROM ANDROID MANIFEST FILE
     
     // Settings Variables
@@ -37,6 +40,62 @@ public class MissileApp extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Get user settings, mode_private --> accessible only by this process
+        settings = getSharedPreferences(PREFERENCES_FILENAME, MODE_PRIVATE);
+        if(settings.getBoolean(PREFERENCES_GPSPROMPT, true)) {
+            //TODO Prompt user to turn on GPS and preference
+        }
+        
+        // Create surface view for cam preview and register callback functions
+        surfaceView = (SurfaceView) findViewById(R.id.camView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        
+        // Trigger Callback functions
+        super.findViewById(R.id.camView).setVisibility(View.INVISIBLE);
+        super.findViewById(R.id.webView).setVisibility(View.INVISIBLE);
     }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MALogger.log(TAG, Log.INFO, "Resuming activity.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MALogger.log(TAG, Log.INFO, "Pausing activity.");
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        MALogger.log(TAG, Log.INFO, "Stopping activity.");
+    }
+    
+    
+    
+    
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+    
     
 }
