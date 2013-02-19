7f37cb7cc39e6e8bfcafb10e4e61a568f02916bf
srkarra <saikarra@gmail.com>
2013-02-19 15:34:53 -0800

    Created necessary variables

--- client/android/src/com/missileapp/android/MissileApp.java
+++ client/android/src/com/missileapp/android/MissileApp.java
@@ -1,10 +1,38 @@
 package com.missileapp.android;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.webkit.WebView;
 import android.app.Activity;
+import android.content.SharedPreferences;
 
 public class MissileApp extends Activity {
+    //TODO [MARKER] REMOVE WAKELOCK FROM ANDROID MANIFEST FILE
     
+    // Settings Variables
+    private static final String TAG = "MainApp";                           // Class Name for Logging
+    private static final String DROIDNB_VARNAME = "AndroidInterface";      // Native Bridge name
+    private static final String DROIDWB_FILENAME =                         // Webview file to load
+            "file:///android_asset/" + "index" + ".html";
+    private static final String PREFERENCES_FILENAME = "SMSFilePref";      // The name of the preference file
+    private static final String PREFERENCES_GPSPROMPT = "DROIDASKGPS";     // The key for asking user for GPS location
+                                                                           // True -> Ask user, False -> skip
+    private static final int CAMERA_ORIENTATION = 90;                      // Camera orientation -> portrait
+    
+
+    // Variables
+    private Camera cam;                        // Camera settings
+    private SurfaceView surfaceView;           // Surface View for layout options
+    private SurfaceHolder surfaceHolder;       // Surface Holder to place Cam Preview
+    private WebView webView;                   // WebView for UI
+    private SharedPreferences settings;        // User Preferences
+    
+    
+    /*********************************
+     * Android OS call back functions
+     *********************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
