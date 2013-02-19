533c0388ebb05e855c9e53c04954d16e4b5e7bf1
srkarra <saikarra@gmail.com>
2013-02-19 15:58:04 -0800

    Removed import

--- client/android/src/com/missileapp/android/MissileApp.java
+++ client/android/src/com/missileapp/android/MissileApp.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.webkit.WebView;
 import android.widget.Toast;
 import android.annotation.SuppressLint;
