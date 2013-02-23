63f707db6d89f93fa03d08c112299e3503c65753
srkarra <saikarra@gmail.com>
2013-02-23 12:04:44 -0800

    Renamed TAG for faster implementation

--- client/android/src/com/missileapp/android/MALogger.java
+++ client/android/src/com/missileapp/android/MALogger.java
@@ -8,18 +8,18 @@ public class MALogger {
     /**
      * Overloaded Constructor, see {@link MALogger#log(String, int, String, Throwable)}
      */
-    public static void log(String tag, int LogType, String msg) {
-        log(tag, LogType, msg, null);
+    public static void log(String TAG, int LogType, String msg) {
+        log(TAG, LogType, msg, null);
     }
     
 	/**
 	 * Logging function, See {@link Log}
-	 * @param tag - Class that needs to log 
+	 * @param TAG - Class that needs to log 
 	 * @param LogType - Type of Log, Look at {@link Log#ASSERT}}
 	 * @param msg - Message to print. Date and Time is automatically added {@link Date#toString()}
 	 * @param tr - Throwable to print {@link Throwable}
 	 */
-	public static void log(String tag, int LogType, String msg, Throwable tr) {
+	public static void log(String TAG, int LogType, String msg, Throwable tr) {
 		// Add Timestamp to msg
 		msg = (msg != null) ? (new Date() +  ": " + msg) : (new Date() +  ": No Msg!");
 		
@@ -29,49 +29,49 @@ public class MALogger {
 				//DEBUG
 				case Log.DEBUG:
 					if(tr != null)
-						Log.d(tag, msg, tr);
+						Log.d(TAG, msg, tr);
 					else
-						Log.d(tag, msg);
+						Log.d(TAG, msg);
 					break;
 				
 				//ERROR
 				case Log.ERROR:
 					if(tr != null)
-						Log.e(tag, msg, tr);
+						Log.e(TAG, msg, tr);
 					else
-						Log.e(tag, msg);
+						Log.e(TAG, msg);
 					break;
 				
 				//Info
 				case Log.INFO:
 					if(tr != null)
-						Log.i(tag, msg, tr);
+						Log.i(TAG, msg, tr);
 					else
-						Log.i(tag, msg);
+						Log.i(TAG, msg);
 					break;
 				
 				//VERBOSE
 				case Log.VERBOSE:
 					if(tr != null)
-						Log.v(tag, msg, tr);
+						Log.v(TAG, msg, tr);
 					else
-						Log.v(tag, msg);
+						Log.v(TAG, msg);
 					break;
 				
 				//WARN
 				case Log.WARN:
 					if(tr != null)
-						Log.w(tag, msg, tr);
+						Log.w(TAG, msg, tr);
 					else
-						Log.w(tag, msg);
+						Log.w(TAG, msg);
 					break;
 	
 				//WTF Happened?! See Log.wtf()
 				default:
 					if(tr != null)
-						Log.wtf(tag, msg, tr);
+						Log.wtf(TAG, msg, tr);
 					else
-						Log.wtf(tag, msg);
+						Log.wtf(TAG, msg);
 					break;
 			}
 		}
