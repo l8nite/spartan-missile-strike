73a84235c57cab4720884b73fc55f1e560695960
Shaun Guth <shaun.guth@gmail.com>
2013-02-25 13:16:43 +0400

    Adds support for session authentication
    Also some minor refactoring to allow routes to install both public and
    authentication-required routes, as well as clean up some minor code
    style issues.

--- service/routes/games.js
+++ service/routes/games.js
@@ -1,22 +1,23 @@
-var install = function (server) {
+function createGame (req, res, next) {
+    res.send(201, 'game created');
+    return next();
+}
+
+function fireMissile (req, res, next) {
+    res.send(200, 'missile fired for game: ' + req.params.id);
+    return next();
+}
+
+function selectBase (req, res, next) {
+    res.send(200, 'select base for game: ' + req.params.id);
+    return next();
+}
+
+module.exports.installAuthenticatedRouteHandlers = function (server) {
     server.post('/games', createGame);
     server.put('/games/:id/fire-missile', fireMissile);
     server.put('/games/:id/select-base', selectBase);
 };
 
-var createGame = function(req, res, next) {
-    res.send(201, 'game created');
-    return next();
+module.exports.installPublicRouteHandlers = function (server) {
 };
-
-var fireMissile = function(req, res, next) {
-    res.send(200, 'missile fired for game: ' + req.params.id);
-    return next();
-};
-
-var selectBase = function(req, res, next) {
-    res.send(200, 'select base for game: ' + req.params.id);
-    return next();
-};
-
-module.exports.install = install;
