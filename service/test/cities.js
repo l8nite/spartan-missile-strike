513dc68fb5fc3c22f9e6b12b371f8c59c5041600
Shaun Guth <shaun.guth@gmail.com>
2013-02-26 05:17:55 +0400

    Refactor unit tests to test authenticated routes
    Also removes the 'facebook test user' creation code from the logic for
    running the unit tests. Instead, a single long-lived access token must
    be manually provided in test/lib/service-client.js

--- service/test/cities.js
+++ service/test/cities.js
@@ -1,10 +1,23 @@
 var should = require('should');
-var client = require('./lib/service-client.js').client;
+var serviceClient = require('./lib/service-client.js');
+
+var authnClient;
+
+before(function () {
+    authnClient = serviceClient.getAuthenticatedClient();
+});
 
 describe('/cities', function() {
-    it('should return a 200', function (done) {
-        client.get('/cities', function(err, req, res, obj) {
-            res.statusCode.should.equal(200);
+    it('should return a 403 when unauthenticated', function (done) {
+        serviceClient.getClient().get('/cities', function (err, req, res) {
+            res.statusCode.should.equal(403);
+            done();
+        });
+    });
+
+    it('should return a 500 not implemented', function (done) {
+        authnClient.get('/cities', function(err, req, res, obj) {
+            res.statusCode.should.equal(500);
             done();
         });
     });
