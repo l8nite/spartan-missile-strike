2ee1ef35789cc183ea1517d236bf7c87b56dc5ae
Jomana Sherif <techjomana@gmail.com>
2013-02-25 20:06:38 -0800

    Checkpoint

--- client/ios/SpartanMissileStrike/SSNativeBridge.m
+++ client/ios/SpartanMissileStrike/SSNativeBridge.m
@@ -11,7 +11,7 @@
 @implementation SSNativeBridge
 
 /**
- "spartan-missile-strike://functionName[calledHOST]:arguments(callbackIdentifier)" 
+ "spartan-missile-strike://functionName[calledHOST]:arguments(callbackIdentifier)"
  schema://hostname/path?key=value1&key2=value2
  /?:&
  */
@@ -27,10 +27,10 @@
 {
     /**
      Bails out if not spartan-missile-strike
-    */
+     */
     NSString* scheme = [url scheme];
     if (![scheme isEqualToString:@"spartan-missile-strike"])
-    {       
+    {
         return YES;
     }
     NSString* query= [url query];
@@ -50,13 +50,23 @@
         for (NSDictionary* result in jsonObject)
         {
             NSString* results= [jsonObject objectForKey:@"sound"];
-             [sa1 playSound:results];
-         }
-                
+            [sa1 playSound:results];
+        }
+        
     }
     return NO;
     
 }
+
+-(void) loadHTMLContent
+{
+    UIWebView* HTMLs = [[UIWebView alloc]init];
+    [HTMLs loadRequest: [NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"ViewFramework-test" ofType:@"html"] isDirectory:NO]]];
+    [HTMLs setScalesPageToFit:YES];
+    HTMLs.backgroundColor = [UIColor clearColor];
+    
+}
+
 
 
 
