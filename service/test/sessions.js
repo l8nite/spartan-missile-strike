63c5e09016a5f3c854e866da134b1951f8ef112d
Shaun Guth <shaun.guth@gmail.com>
2013-02-26 05:17:55 +0400

    Refactor unit tests to test authenticated routes
    Also removes the 'facebook test user' creation code from the logic for
    running the unit tests. Instead, a single long-lived access token must
    be manually provided in test/lib/service-client.js

--- service/test/sessions.js
+++ service/test/sessions.js
@@ -1,11 +1,15 @@
 var should = require('should');
-var svctest = require('./lib/service-client.js');
-var fbtest = require('./lib/test-user.js');
-var client = svctest.client;
+var serviceClient = require('./lib/service-client.js');
+
+var client;
+var authnClient;
+
+before(function() {
+    client = serviceClient.getClient();
+    authnClient = serviceClient.getAuthenticatedClient();
+});
 
 describe('/sessions', function() {
-    this.timeout(15000);
-
     it('should return a 400 when an invalid facebook_access_token parameter is sent', function (done) {
         client.post('/sessions', { facebook_access_token: "abcdefg" }, function(err, req, res, obj) {
             res.statusCode.should.equal(400);
@@ -14,34 +18,25 @@ describe('/sessions', function() {
     });
 
     describe('creating a valid session', function(done) {
-        var err, req, res, obj, fbuser;
-
+        var response, data;
         before(function(done) {
-            fbuser = fbtest.getFacebookTestData().user;
-            console.log(fbuser);
-
-            client.post('/sessions', { facebook_access_token: fbuser.access_token }, function(er, rq, rs, ob) {
-                err = er;
-                req = rq;
-                res = rs;
-                obj = ob;
-
-                console.log(obj);
-
+            client.post('/sessions', { facebook_access_token: serviceClient.fbTestUserAccessToken }, function(err, req, res, obj) {
+                response = res;
+                data = obj;
                 done();
             });
         });
 
         it('should return a 201 when a valid facebook_access_token parameter is sent', function (done) {
-            res.statusCode.should.equal(201);
+            response.statusCode.should.equal(201);
             done();
         });
 
         it('should have valid properties in the returned object', function (done) {
-            obj.should.have.property('session');
-            obj.session.should.have.property('id');
-            obj.should.have.property('user');
-            obj.user.facebook.should.have.property('access_token');
+            data.should.have.property('session');
+            data.session.should.have.property('id');
+            data.should.have.property('user');
+            data.user.facebook.should.have.property('access_token');
             done();
         });
     });
@@ -55,9 +50,16 @@ describe('/sessions', function() {
 });
 
 describe('/sessions/:id', function() {
-    it('should return a 403 if no SMSS-Session-ID is sent', function (done) {
+    it('should return a 403 for unauthenticated clients', function (done) {
         client.del('/sessions/fe4d37b8-ff94-452d-ae6a-e31e30bbafd9', function(err, req, res, obj) {
-            res.statusCode.should.equal(200);
+            res.statusCode.should.equal(403);
+            done();
+        });
+    });
+
+    it('should return a 500 not implemented', function (done) {
+        authnClient.del('/sessions/fe4d37b8-ff94-452d-ae6a-e31e30bbafd9', function(err, req, res, obj) {
+            res.statusCode.should.equal(500);
             done();
         });
     });
