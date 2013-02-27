bd75613efbcf1ead1be8d37492c66b3cf913626c
Shaun Guth <shaun.guth@gmail.com>
2013-02-27 13:20:57 +0400

    /users/:id works to fetch user details
    For now the resulting object is white-listed to only contain limited
    facebook information.
    
    Updated the unit tests to cover new functionality

--- service/test/users.js
+++ service/test/users.js
@@ -9,9 +9,24 @@ describe('/users', function () {
         client.login('Service General Unit Tests', done);
     });
 
-    describe('/users/:id', function() {
+    describe('GET /users/:id', function() {
+        it('should return user details', function (done) {
+            var path = '/users/' + encodeURIComponent(client.user.id);
+            client.get(path, function(err, req, res, obj) {
+                should.not.exist(err);
+                should.exist(obj);
+                obj.should.have.property('id');
+                obj.id.should.equal(client.user.id);
+                obj.facebook.should.not.have.property('access_token');
+                res.statusCode.should.equal(200);
+                done();
+            });
+        });
+    });
+
+    describe('PUT /users/:id', function () {
         it('should return a 500 not implemented', function (done) {
-            client.get('/users/fe4d37b8-ff94-452d-ae6a-e31e30bbafd9', function(err, req, res, obj) {
+            client.put('/users/user%3A00000000-0000-0000-0000-000000000000', {}, function(err, req, res, obj) {
                 res.statusCode.should.equal(500);
                 done();
             });
@@ -20,7 +35,7 @@ describe('/users', function () {
 
     describe('/users/:id/games', function() {
         it('should return a 500 not implemented', function (done) {
-            client.get('/users/fe4d37b8-ff94-452d-ae6a-e31e30bbafd9/games', function(err, req, res, obj) {
+            client.get('/users/user%3A00000000-0000-0000-0000-000000000000/games', function(err, req, res, obj) {
                 res.statusCode.should.equal(500);
                 done();
             });
@@ -29,8 +44,26 @@ describe('/users', function () {
 
     describe('/users/:id/opponents', function() {
         it('should return a 500 not implemented', function (done) {
-            client.get('/users/fe4d37b8-ff94-452d-ae6a-e31e30bbafd9/opponents', function(err, req, res, obj) {
+            client.get('/users/user%3A00000000-0000-0000-0000-000000000000/opponents', function(err, req, res, obj) {
                 res.statusCode.should.equal(500);
+                done();
+            });
+        });
+    });
+
+    describe('invalid id', function () {
+        it('should return a 400 bad request', function (done) {
+            client.get('/users/user%3Ainvalid', function (err, req, res, obj) {
+                res.statusCode.should.equal(400);
+                done();
+            });
+        });
+    });
+
+    describe('GET /users/:id with a valid but nonexistent id', function () {
+        it('should return a 404 not found', function (done) {
+            client.get('/users/user%3A00000000-0000-0000-0000-000000000000', function (err, req, res, obj) {
+                res.statusCode.should.equal(404);
                 done();
             });
         });
