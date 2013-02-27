e526acbe0397d00c2802b9bcf12611445750bcb1
Shaun Guth <shaun.guth@gmail.com>
2013-02-27 13:20:57 +0400

    /users/:id works to fetch user details
    For now the resulting object is white-listed to only contain limited
    facebook information.
    
    Updated the unit tests to cover new functionality

--- service/routes/users.js
+++ service/routes/users.js
@@ -1,32 +1,86 @@
-function showUser (req, res, next) {
-    res.send(500, 'not implemented');
-    //, 'user details for userid: ' + req.params.id);
-    return next();
+var restify = require('restify');
+var redis = require('../lib/database.js');
+var async = require('async');
+var _ = require('underscore');
+
+function showUser (msUserId, done) {
+    async.waterfall([
+        function (next) { next(null, msUserId); },
+        _fetchUserFromDatabase,
+        _renderUserDetails
+    ], done);
 }
 
-function updateUser (req, res, next) {
-    res.send(500, 'not implemented');
-    // , 'updated session with id: ' + req.params.id);
-    return next();
+function _renderUserDetails (msUser, next) {
+    var sanitizedUser = _.pick(msUser, 'id', 'username');
+    var sanitizedFacebook = _.pick(msUser.facebook, 'id', 'name', 'link');
+
+    sanitizedUser.facebook = sanitizedFacebook;
+
+    next(null, 200, sanitizedUser);
 }
 
-function listGames (req, res, next) {
-    res.send(500, 'not implemented');
-    // , 'list of games for user with id: ' + req.params.id);
-    return next();
+function _fetchUserFromDatabase (msUserId, next) {
+    redis.client.get(msUserId, function (err, msUserSerialized) {
+        if (err) {
+            return next(err, 500);
+        }
+
+        if (msUserSerialized === null) {
+            return next(new restify.ResourceNotFoundError(), 404);
+        }
+
+        next(null, JSON.parse(msUserSerialized));
+    });
 }
 
-function listOpponents (req, res, next) {
-    res.send(500, 'not implemented');
-    //, 'list of opponents for user with id: ' + req.params.id);
-    return next();
+function updateUser (msUserId, done) {
+    return done('not implemented', 500);
+}
+
+function listGames (msUserId, done) {
+    return done('not implemented', 500);
+}
+
+function listOpponents (msUserId, done) {
+    return done('not implemented', 500);
+}
+
+
+function userIdRequiredHandler (handler) {
+    return function (request, response, done) {
+        async.waterfall([
+            function (next) { next(null, request); }, // enables arguments to first callback
+            _validateUserIdParameter,
+            handler,
+        ],
+
+        function (err, code, body) {
+            if (code) {
+                response.send(code, body);
+            }
+            else {
+                response.send(500, err);
+            }
+
+            done();
+        });
+    };
+}
+
+function _validateUserIdParameter (request, next) {
+    if (!/user:[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}/.test(request.params.id)) {
+        return next(new restify.InvalidArgumentError(), 400);
+    }
+
+    return next(null, request.params.id);
 }
 
 module.exports.installAuthenticatedRouteHandlers = function (server) {
-    server.get('/users/:id', showUser);
-    server.put('/users/:id', updateUser);
-    server.get('/users/:id/games', listGames);
-    server.get('/users/:id/opponents', listOpponents);
+    server.get('/users/:id', userIdRequiredHandler(showUser));
+    server.put('/users/:id', userIdRequiredHandler(updateUser));
+    server.get('/users/:id/games', userIdRequiredHandler(listGames));
+    server.get('/users/:id/opponents', userIdRequiredHandler(listOpponents));
 };
 
 module.exports.installPublicRouteHandlers = function (server) {
