440e14293ee49f31ed356b801b12c977a9bc068c
srkarra <saikarra@gmail.com>
2013-03-02 10:04:06 -0800

    added foreground map for audio

--- client/android/src/com/missileapp/android/res/MediaManager.java
+++ client/android/src/com/missileapp/android/res/MediaManager.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.util.SparseIntArray;
 
 import com.missileapp.android.BagOfHolding;
@@ -25,12 +26,11 @@ public class MediaManager {
 	
 	// Sound loading variables
 	private static final String TAG = "MediaManager";
-	private static final int MAX_FILES_PLAYED_SIMULTANEOUS =  6;           // MAX # of audio files playing at any given time TODO VERIFY THIS 
+	private static final int MAX_FILES_PLAYED_SIMULTANEOUS =  10;          // MAX # of audio files playing at any given time TODO VERIFY THIS 
 	private static final int SOURCE_QUALITY = 0;                           // QUALITY, currently unused, future android operation
 	private static final int SOURCE_PRIORITY = 1;                          // Load Priority, currently unused, future android operation
-	private static final float SPEAKER_VOLUME_LEFT = 1.0f;                 // Left speaker volume 100%
-	private static final float SPEAKER_VOLUME_RIGHT = 1.0f;                // Right speaker volume 100%
-	private static final float PLAY_RATE = 1.0f;                           // Play Rate - Normal
+	private static final float DEFAULT_SPEAKER_VOLUME = 1.0f;              // Default Speaker Volume
+	private static final float DEFAULT_PLAY_RATE = 1.0f;                           // Play Rate - Normal
 	private static final int DEFAULT_PRIORITY = 2;                         // Default Priority
 	private static final int BACKGROUND_PRIORITY = 1;                      // Background Priority
 	private static final int PLAYBACK_DEFAULT_VALUE = 0;                   // Default number of playback times - 1
@@ -41,7 +41,10 @@ public class MediaManager {
 	private BagOfHolding variables;            // Variables
     private SoundPool soundPool;               // Sound Pool Manager
     private SparseIntArray soundMap;           // Maps raw identifer to sound pool identifier
-
+    private SparseBooleanArray foregroundMap;  // Keeps track of what's in the foreground
+    private float foregroundVolume;            // foreground volume
+    private float backgroundVolume;            // background volume
+    
     /**
      * Media Manager to Play/Mute/Stop Audio 
      * @param variables {@link BagOfHolding} data
@@ -49,7 +52,12 @@ public class MediaManager {
     public MediaManager(BagOfHolding variables) {
         this.variables = variables;
         soundMap = new SparseIntArray();
+        foregroundMap = new SparseBooleanArray();
         soundPool = new SoundPool(MAX_FILES_PLAYED_SIMULTANEOUS, AudioManager.STREAM_MUSIC, SOURCE_QUALITY);
+        
+        // Get Volume Data
+        foregroundVolume = variables.getSettings().getFloat("backVolume", DEFAULT_SPEAKER_VOLUME);
+        backgroundVolume = variables.getSettings().getFloat("backVolume", DEFAULT_SPEAKER_VOLUME);
         
         // Load Files
         soundMap.put(FX_BACKGROUND_MUSIC, soundPool.load(variables.getMissileApp(), FX_BACKGROUND_MUSIC, SOURCE_PRIORITY));
@@ -58,6 +66,37 @@ public class MediaManager {
         soundMap.put(FX_MISSILE_LAUNCHED, soundPool.load(variables.getMissileApp(), FX_MISSILE_LAUNCHED, SOURCE_PRIORITY));
         soundMap.put(FX_PAGE_TRANSITION, soundPool.load(variables.getMissileApp(), FX_PAGE_TRANSITION, SOURCE_PRIORITY));
         soundMap.put(FX_TURRET_MOVING, soundPool.load(variables.getMissileApp(), FX_TURRET_MOVING, SOURCE_PRIORITY));
+        
+        //TODO Implement listener for load
+    }
+    
+    /**
+     * Transltes
+     * @param nativeBridgeID
+     * @return
+     */
+    public int translateSoundIDToRawPointer(String nativeBridgeID) {
+    	if(nativeBridgeID.equalsIgnoreCase("background_music")){
+			return FX_BACKGROUND_MUSIC;
+		}
+		else if(nativeBridgeID.equalsIgnoreCase("explosion")){
+			return FX_EXPLOSION;
+		}
+		else if(nativeBridgeID.equalsIgnoreCase("camera_focused")){
+			return FX_CAMERA_FOCUSED;
+		}
+		else if(nativeBridgeID.equalsIgnoreCase("missile_launched")){
+			return FX_MISSILE_LAUNCHED;
+		}
+		else if(nativeBridgeID.equalsIgnoreCase("page_transition")){
+			return FX_PAGE_TRANSITION;
+		}
+		else if(nativeBridgeID.equalsIgnoreCase("turret_moving")){
+			return FX_TURRET_MOVING;
+		}
+		else {
+			return -1;
+		}
     }
     
     
@@ -67,29 +106,20 @@ public class MediaManager {
      * @return sound pool mapping stream id
      */
     public int getSoundPoolMappingFromRawID(String nativeBridgeID) {
-		if(nativeBridgeID.equalsIgnoreCase("background_music")){
-			return soundMap.get(FX_BACKGROUND_MUSIC);
-		}
-		else if(nativeBridgeID.equalsIgnoreCase("explosion")){
-			return soundMap.get(FX_EXPLOSION);
-		}
-		else if(nativeBridgeID.equalsIgnoreCase("camera_focused")){
-			return soundMap.get(FX_CAMERA_FOCUSED);
-		}
-		else if(nativeBridgeID.equalsIgnoreCase("missile_launched")){
-			return soundMap.get(FX_MISSILE_LAUNCHED);
-		}
-		else if(nativeBridgeID.equalsIgnoreCase("page_transition")){
-			return soundMap.get(FX_PAGE_TRANSITION);
-		}
-		else if(nativeBridgeID.equalsIgnoreCase("turret_moving")){
-			return soundMap.get(FX_TURRET_MOVING);
-		}
-		else {
-			return -1;
-		}
+		return getSoundPoolMappingFromRawID(translateSoundIDToRawPointer(nativeBridgeID));
 	}
-
+    
+    
+    /**
+     * Returns the sound pool id given the raw uri to the file
+     * @param rawID the raw id/uri of the sound file
+     * @return sound pool mapping stream id
+     */
+    public int getSoundPoolMappingFromRawID(int rawID) {
+    	return soundMap.get(rawID);
+    }
+    
+    
     
     /**
      * TODO UPDATE VOLUME SETTINGS
@@ -100,6 +130,10 @@ public class MediaManager {
      *            loop - ture to loop the audio
      */
     public void playSound(String options) {
+    	float volume;
+		int priority;
+		int loopTimes;
+		
         try {
         	MALogger.log(TAG, Log.INFO, "Play Sound");
         	
@@ -110,13 +144,19 @@ public class MediaManager {
 			boolean loop = playSound.getBoolean("loop");
 			
 			//TODO: isn't background inherently loop ?
-			//TODO: update volume
+			// Set up volume, loop time, priority, and play rate
+			if(foreground) {
+				volume = foregroundVolume;
+				priority  = DEFAULT_PRIORITY;
+			}
+			else {
+				volume = backgroundVolume;
+				priority = BACKGROUND_PRIORITY;
+			}
+			loopTimes = (loop) ? PLAYBACK_LOOP_VALUE : PLAYBACK_DEFAULT_VALUE;
 			
 			// Play sound with options
-			soundPool.play(soundPoolID, SPEAKER_VOLUME_LEFT, SPEAKER_VOLUME_RIGHT,
-					(foreground) ? DEFAULT_PRIORITY : BACKGROUND_PRIORITY,
-					(loop) ? PLAYBACK_LOOP_VALUE : PLAYBACK_DEFAULT_VALUE,
-					PLAY_RATE);
+			soundPool.play(soundPoolID, volume, volume, priority, loopTimes, DEFAULT_PLAY_RATE);
 		}
         catch (JSONException e) {
 			MALogger.log(TAG, Log.ERROR, "Could not parse options" , e);
@@ -172,4 +212,15 @@ public class MediaManager {
     }
     
     //TODO: MAKE volume function
+    public void setForegroundVolume(float volume) {
+    	foregroundVolume = volume;
+    	
+    	//TODO: Change Volume for forground
+    	for (int i = 0; i < foregroundMap.size(); i++) {
+		}
+    }
+    
+    public void setBackgroundVolume(float volume) {
+    	
+    }
 }
