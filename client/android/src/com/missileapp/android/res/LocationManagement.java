f4b33fcece14fc5106c5b8a6e8855e3cb3596cad
srkarra <saikarra@gmail.com>
2013-03-05 18:47:23 -0800

    Added Simple Location Management Class

---
+++ client/android/src/com/missileapp/android/res/LocationManagement.java
@@ -0,0 +1,21 @@
+package com.missileapp.android.res;
+
+import com.missileapp.android.BagOfHolding;
+
+public class LocationManagement {
+	// DATA
+    private static final String TAG = "Location";                 // TAG for logging
+    private BagOfHolding variables;                               // Variable bag
+    
+    /**
+     * Location Manager for Android 
+     * @param variables - Variables Bag
+     */
+	public LocationManagement(BagOfHolding variables) {
+		super();
+		this.variables = variables;
+	}
+    
+    
+
+}
