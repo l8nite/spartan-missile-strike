9fd9e2bb5df617a83eff2d87386bcd388adb2aef
srkarra <saikarra@gmail.com>
2013-03-05 19:22:21 -0800

    Implement Location Listener

--- client/android/src/com/missileapp/android/res/LocationManagement.java
+++ client/android/src/com/missileapp/android/res/LocationManagement.java
@@ -1,8 +1,12 @@
 package com.missileapp.android.res;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
 import com.missileapp.android.BagOfHolding;
 
-public class LocationManagement {
+public class LocationManagement implements LocationListener {
 	// DATA
     private static final String TAG = "Location";                 // TAG for logging
     private BagOfHolding variables;                               // Variable bag
@@ -15,7 +19,42 @@ public class LocationManagement {
 		super();
 		this.variables = variables;
 	}
-    
-    
+	
+	
+	/**
+	 * Location Provider Created
+	 */
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Location Provider Destroyed
+	 */
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Location Changed
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+	}
+	
+	
+	/**
+	 * Location Service Status
+	 */
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
