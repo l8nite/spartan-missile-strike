7107dfb81615fa4c0e08896f59abdc1b58a3cbbe
srkarra <saikarra@gmail.com>
2013-03-09 13:34:12 -0800

    Updated BagOfHolding to track MissileApp Enabled status for location services and splash screen

--- client/android/src/com/missileapp/android/BagOfHolding.java
+++ client/android/src/com/missileapp/android/BagOfHolding.java
@@ -12,32 +12,36 @@ import android.os.Vibrator;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.webkit.WebView;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 
 /**
  * Bag of Holding - Holds the application data and variables
  */
 public class BagOfHolding extends Application {
-    private static BagOfHolding boh;
+//    private static BagOfHolding boh;
     
     // Primary Classes
     private MissileApp missileApp;             // MissileApp instance
     private AndroidBridge droidBridge;         // Android Interface to the WebView
     
     // Android Views
+    private CheckBox locationCheckBox;         // Checkbox for gps prompts
     private SurfaceView surfaceView;           // Surface View for layout options
     private SurfaceHolder surfaceHolder;       // Surface Holder to place Cam Preview
     private WebView webView;                   // WebView for UI
     private ImageView splashScreen;            // ImageView
     
     // Resource variables
-    private SharedPreferences settings;        // System User Preferences
-    private UserPreferences userPrefs;         // Droid Native Bridge user prefs implementation
-    private Vibrator vibrator;                 // Vibrator unit
-    private FireScreen fireScreen;             // FireScreen, contains the camear framework
-    private MediaManager mediaManager;         // Media Manager that keeps track of all files
-    private LocationManager locationManager;   // Android System Service giving access to location info
-    private LocationManagement locationManage; // MissileApp Local Location Managements 
+    private boolean isEnabled;                        // Application is enabled
+    private boolean hideSplash;                       // Hide Splash registered before enabled;
+    private SharedPreferences settings;               // System User Preferences
+    private UserPreferences userPrefs;                // Droid Native Bridge user prefs implementation
+    private Vibrator vibrator;                        // Vibrator unit
+    private FireScreen fireScreen;                    // FireScreen, contains the camear framework
+    private MediaManager mediaManager;                // Media Manager that keeps track of all files
+    private LocationManager locationManager;          // Android System Service giving access to location info
+    private LocationManagement locationManagement;    // MissileApp Local Location Managements 
     
     /*********************************
      * Android OS call back functions
@@ -48,7 +52,8 @@ public class BagOfHolding extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        BagOfHolding.boh = this;
+        this.isEnabled = false;
+//        BagOfHolding.boh = this;
     }
     
     
@@ -81,13 +86,13 @@ public class BagOfHolding extends Application {
     //          Primary Android Classes
     //
     ///////////////////////////////////////////////
-    /**
-     * Returns this class
-     * @return this class holding all the variables
-     */
-    public static BagOfHolding getInstance() {
-        return boh;
-    }
+//    /**
+//     * Returns this class
+//     * @return this class holding all the variables
+//     */
+//    public static BagOfHolding getInstance() {
+//        return boh;
+//    }
     
     /**
      * Returns an instance of the {@link MissileApp}
@@ -131,16 +136,25 @@ public class BagOfHolding extends Application {
     //              Android Views
     //
     ///////////////////////////////////////////////
+    
+    public CheckBox getLocationCheckBox() {
+		return locationCheckBox;
+	}
+
+
+	public void setLocationCheckBox(CheckBox locationCheckBox) {
+		this.locationCheckBox = locationCheckBox;
+	}
 
     /**
      * Returns the {@link SurfaceView} where the camera preview is drawn on
-     * @return the {@link SurfaceView} intance
+     * @return the {@link SurfaceView} instance
      */
     public SurfaceView getSurfaceView() {
         return surfaceView;
     }
-
-    /**
+    
+	/**
      * Sets the {@link SurfaceView} where the camera preview is drawn on
      * @param surfaceView the {@link SurfaceView} to be drawn on
      */
@@ -211,8 +225,40 @@ public class BagOfHolding extends Application {
     //
     ///////////////////////////////////////////////
 
+    /**
+     * Is MissileApp enabled? (location turned on)
+     * @return true if enabled, else false
+     */
+    public boolean isEnabled() {
+		return isEnabled;
+	}
 
     /**
+     * Set enabled status of MissileApp
+     * @param isEnabled, true to enable
+     */
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+	
+	/**
+	 * Set to hide splash
+	 * @return true to hide splash, else false
+	 */
+	public boolean isHideSplash() {
+		return hideSplash;
+	}
+	
+	/**
+	 * Set to hide splash after missile app is enabled
+	 * @return true to hide splash, else false
+	 */
+	public void setHideSplash(boolean hideSplash) {
+		this.hideSplash = hideSplash;
+	}
+
+	
+	/**
      * A User's preferences in terms of {@link SharedPreferences}
      * @return the user's preferences
      */
@@ -316,7 +362,7 @@ public class BagOfHolding extends Application {
 	 * @return {@link LocationManagement} implementation
 	 */
 	public LocationManagement getLocationManagement() {
-		return locationManage;
+		return locationManagement;
 	}
 	
 	/**
@@ -324,6 +370,6 @@ public class BagOfHolding extends Application {
 	 * @param locationManagement {@link LocationManagement} implementation
 	 */
 	public void setLocationManagement(LocationManagement locationManagement) {
-		this.locationManage = locationManagement;
+		this.locationManagement = locationManagement;
 	}
 }
