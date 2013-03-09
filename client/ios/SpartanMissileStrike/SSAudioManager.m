910652dcbe2a90e2a14bef4d2cca141307feea4c
Jomana Sherif <techjomana@gmail.com>
2013-03-09 09:23:33 -0800

    Camera core code is in there, location and Motion core code is in there, Camera not opening but loading.

--- client/ios/SpartanMissileStrike/SSAudioManager.m
+++ client/ios/SpartanMissileStrike/SSAudioManager.m
@@ -23,6 +23,8 @@
     for (NSString* key in filenameForSound)
     {
         
+        
+        
         NSLog(@"key in Filenameforsound: %@",key);
         NSURL* url = [[NSBundle mainBundle] URLForResource: [filenameForSound objectForKey:key]  withExtension:@"mp3"];
         NSLog(@"ENTERED url dscrip %@", url.description);
@@ -30,34 +32,34 @@
         
         AVAsset *asset = [AVURLAsset URLAssetWithURL:url options:nil];
         AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
-
-        player = [self createAVAudioPlayer];
-      // NSError *theerr;
         
-       [player initWithContentsOfURL:url error:nil];
-       // player = [AVPlayer playerWithPlayerItem:playerItem];
+        player = [self createAVAudioPlayer];
+        // NSError *theerr;
+        
+        [player initWithContentsOfURL:url error:nil];
+        // player = [AVPlayer playerWithPlayerItem:playerItem];
         
         [playerForSound setObject:player forKey:key];
         
-       ///ARC requirement need proper mem binding
-   // NSLog(@"Player URL %@",player.url);
+        ///ARC requirement need proper mem binding
+        // NSLog(@"Player URL %@",player.url);
         
         /*
          Background Music
          */
         // get the nsobject pointer from plist dictionary
-//        
-//        NSDictionary *bckDict = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource: @"sounds" ofType: @"plist"] ];
-//        NSLog(@"dct; %@",bckDict);
-//        
-//        NSURL* url = [[NSBundle mainBundle] URLForResource: [bckDict objectForKey:@"MODERATO"]  withExtension:@"mp3"];
-//        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:url];
-//        NSLog(@"playeritem: %@",playerItem.description);
-//        AVPlayer* av1 = [AVPlayer playerWithPlayerItem:playerItem];
-//        [av1 play];
-//        NSLog(@"av1 :%lld",av1.currentItem.duration.value);
-//
-//        
+        //
+        //        NSDictionary *bckDict = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource: @"sounds" ofType: @"plist"] ];
+        //        NSLog(@"dct; %@",bckDict);
+        //
+        //        NSURL* url = [[NSBundle mainBundle] URLForResource: [bckDict objectForKey:@"MODERATO"]  withExtension:@"mp3"];
+        //        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:url];
+        //        NSLog(@"playeritem: %@",playerItem.description);
+        //        AVPlayer* av1 = [AVPlayer playerWithPlayerItem:playerItem];
+        //        [av1 play];
+        //        NSLog(@"av1 :%lld",av1.currentItem.duration.value);
+        //
+        //
     }
     
     return self;
@@ -66,18 +68,18 @@
 -(void)playSound:(NSString*)identifier
 {
     NSLog(@"identifier %@", identifier.description);
-
+    
     
     // get the nsobject pointer from playerforsound dictionary
     NSObject* obj1 = [playerForSound objectForKey:identifier];
     // cast the nsobject pointer to an avudioplayer pointer
     AVAudioPlayer* av1= (AVAudioPlayer*)obj1;
-   
+    
     [av1 play];
     
 }
 /**
-    Factory Method
+ Factory Method
  */
 
 -(AVAudioPlayer*)createAVAudioPlayer
@@ -86,5 +88,5 @@
 }
 
 @end
- 
+
 
