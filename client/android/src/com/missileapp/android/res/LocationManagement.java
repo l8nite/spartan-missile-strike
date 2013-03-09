1c28b26dc116d2f59b9e5dbf2bec24ddd3517198
srkarra <saikarra@gmail.com>
2013-03-09 09:22:19 -0800

    Temp ignore

--- client/android/src/com/missileapp/android/res/LocationManagement.java
+++ client/android/src/com/missileapp/android/res/LocationManagement.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.missileapp.android.BagOfHolding;
 
+@SuppressWarnings("unused")
 public class LocationManagement implements LocationListener {
 	// DATA
     private static final String TAG = "Location";                 // TAG for logging
