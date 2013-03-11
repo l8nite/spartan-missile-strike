d1c837e70bb2e3f6c36a88169edbd2002146619b
srkarra <saikarra@gmail.com>
2013-03-10 23:29:18 -0700

    Added callback listener

--- client/android/src/com/missileapp/android/res/Gyro.java
+++ client/android/src/com/missileapp/android/res/Gyro.java
@@ -1,14 +1,20 @@
 package com.missileapp.android.res;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 import com.missileapp.android.BagOfHolding;
 
 @SuppressWarnings("unused")
-public class Gyro {
+public class Gyro implements SensorEventListener{
     // DATA
     private static final String TAG = "Gyro";               // TAG for logging
     private BagOfHolding variables;                         // Variable bag
     private boolean isNativeBridgeSubscribed;               // true of native bridge is subscribed
-    private String callbackID;                              // call back ident for native brigde
+    private String callbackID;                              // call back ident for native bridge
+    private Sensor gyro;                                    // Gyro sensor implementation
     
     /**
      * Gyroscrope class for Native Bridge
@@ -27,9 +33,27 @@ public class Gyro {
         try {
             isNativeBridgeSubscribed = Boolean.valueOf(activate);
             this.callbackID = callbackID;
+            
+            if(isNativeBridgeSubscribed) {
+                
+            }
+            else {
+
+            }
+            
         }
         catch (Exception e) {
             isNativeBridgeSubscribed = false;
         }
+    }
+
+    /** Accuracy changes */
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+    /** 
+     * Sensor 
+     */
+    public void onSensorChanged(SensorEvent event) {
+        
     }
 }
