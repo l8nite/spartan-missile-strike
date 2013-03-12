e7af2ed27fbd9bcd0e1399dab4ed46ab32b4cab8
Shaun Guth <shaun.guth@gmail.com>
2013-03-11 20:33:24 -0700

    add SSSplashScreenViewController

---
+++ client/ios/SpartanMissileStrike/SSSplashScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSSplashScreenViewController.m
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/11/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import "SSSplashScreenViewController.h"
+
+@interface SSSplashScreenViewController ()
+
+@end
+
+@implementation SSSplashScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
