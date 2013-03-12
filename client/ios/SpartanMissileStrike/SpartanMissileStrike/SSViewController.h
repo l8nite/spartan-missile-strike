c9881d3d29b6329d66dd5aa032037aab8691cefa
Shaun Guth <shaun.guth@gmail.com>
2013-03-11 18:17:24 -0700

    File -> New Project

---
+++ client/ios/SpartanMissileStrike/SpartanMissileStrike/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/11/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
