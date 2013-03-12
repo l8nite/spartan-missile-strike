481cbc1e837b7c3433b185f64e38d6a700410921
Shaun Guth <shaun.guth@gmail.com>
2013-03-11 21:41:48 -0700

    initial UIWebView and HTML content loading

--- client/ios/SpartanMissileStrike/SSMainViewController.m
+++ client/ios/SpartanMissileStrike/SSMainViewController.m
@@ -8,22 +8,34 @@
 
 #import "SSMainViewController.h"
 
-@interface SSMainViewController ()
-
-@end
-
 @implementation SSMainViewController
+
+@synthesize webView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self initializeHtmlContent];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload
+{
+    [self setWebView:nil];
+    [super viewDidUnload];
+}
+
+- (void)initializeHtmlContent
+{
+    NSURL *indexURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"html/View/ViewFramework-test" ofType:@"html"] isDirectory:NO];
+    NSURLRequest *initialLoadRequest = [NSURLRequest requestWithURL:indexURL];
+    [webView loadRequest:initialLoadRequest];
 }
 
 @end
