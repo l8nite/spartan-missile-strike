5c7d4432f924a7f83bf9630fb2185add38f119b3
Shaun Guth <shaun.guth@gmail.com>
2013-03-12 14:07:03 -0700

    facebook authentication flow working properly

--- client/ios/SpartanMissileStrike/SSFacebookManager.h
+++ client/ios/SpartanMissileStrike/SSFacebookManager.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol SSFacebookManagerDelegate;
+
 @interface SSFacebookManager : NSObject
+
+@property (unsafe_unretained) id<SSFacebookManagerDelegate> delegate;
+
+-(BOOL)isLoggedIn;
+-(void)openSession;
+-(void)handleDidBecomeActive;
+-(BOOL)handleOpenURL:(NSURL *)url;
+-(NSString *)accessToken;
 
 @end
--- client/ios/SpartanMissileStrike/SSFacebookManager.m
+++ client/ios/SpartanMissileStrike/SSFacebookManager.m
@@ -7,8 +7,64 @@
 //
 
 #import "SSFacebookManager.h"
+#import "SSFacebookManagerDelegate.h"
 #import <FacebookSDK/FacebookSDK.h>
 
 @implementation SSFacebookManager
 
+@synthesize delegate;
+
+-(void)openSession
+{
+    NSLog(@"calling openSession");
+    [FBSession openActiveSessionWithReadPermissions:nil
+                                       allowLoginUI:YES
+                                  completionHandler:
+     ^(FBSession *session,
+       FBSessionState state, NSError *error) {
+         [self sessionStateChanged:session state:state error:error];
+     }];
+}
+
+-(BOOL)isLoggedIn
+{
+    return FBSession.activeSession.state == FBSessionStateCreatedTokenLoaded;
+}
+
+-(void)handleDidBecomeActive
+{
+    [FBSession.activeSession handleDidBecomeActive];
+}
+
+-(BOOL)handleOpenURL:(NSURL *)url
+{
+    return [FBSession.activeSession handleOpenURL:url];
+}
+
+- (void)sessionStateChanged:(FBSession *)session
+                      state:(FBSessionState) state
+                      error:(NSError *)error
+{
+    switch (state) {
+        case FBSessionStateOpen:
+            [delegate facebookUserDidLogIn];
+            break;
+        case FBSessionStateClosed:
+        case FBSessionStateClosedLoginFailed:
+            [FBSession.activeSession closeAndClearTokenInformation];
+            [delegate facebookUserDidLogOut];
+            break;
+        default:
+            break;
+    }
+    
+    if (error) {
+        NSLog(@"sessionStateChanged Error: %@", error);
+    }
+}
+
+-(NSString *)accessToken
+{
+    return FBSession.activeSession.accessTokenData.accessToken;
+}
 @end
