fe2533a46845d0f09666cee040d19bafe32fb230
Shaun Guth <shaun.guth@gmail.com>
2013-03-12 12:32:45 -0700

    integrate FacebookSDK

---
+++ client/ios/SpartanMissileStrike/SSFacebookManager.m
@@ -0,0 +1,14 @@
+//
+//  SSFacebookManager.m
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/12/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import "SSFacebookManager.h"
+#import <FacebookSDK/FacebookSDK.h>
+
+@implementation SSFacebookManager
+
+@end
