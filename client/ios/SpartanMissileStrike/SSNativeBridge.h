2d14460ea54156aaf4b585c9e135dcd575fccb67
Shaun Guth <shaun.guth@gmail.com>
2013-03-11 23:13:54 -0700

    Initial NativeBridge implementation

---
+++ client/ios/SpartanMissileStrike/SSNativeBridge.h
@@ -0,0 +1,15 @@
+//
+//  SSNativeBridge.h
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/11/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSNativeBridge : NSObject <UIWebViewDelegate>
+
+@end
+
+
