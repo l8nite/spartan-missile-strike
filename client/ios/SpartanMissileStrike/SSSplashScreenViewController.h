eb2f014872f448f6a6c1c732c70104d731955dcf
Shaun Guth <shaun.guth@gmail.com>
2013-03-11 20:33:24 -0700

    add SSSplashScreenViewController

---
+++ client/ios/SpartanMissileStrike/SSSplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSSplashScreenViewController.h
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/11/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSSplashScreenViewController : UIViewController
+
+@end
