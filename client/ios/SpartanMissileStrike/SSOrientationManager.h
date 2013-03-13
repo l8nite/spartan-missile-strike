88ff2bb4bcf9372e6a5b601325ee60c0e5ea78ad
Shaun Guth <shaun.guth@gmail.com>
2013-03-13 16:42:32 -0700

    new OrientationManager class

---
+++ client/ios/SpartanMissileStrike/SSOrientationManager.h
@@ -0,0 +1,13 @@
+//
+//  SSOrientationManager.h
+//  SpartanMissileStrike
+//
+//  Created by Shaun Guth on 3/13/13.
+//  Copyright (c) 2013 missileapp.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSOrientationManager : NSObject
+
+@end
