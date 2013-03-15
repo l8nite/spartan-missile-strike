eead1c83af689e45d8c211a006923489be01ed41
Christopher Cook <cucook@gmail.com>
2013-03-14 18:59:29 -0700

    Changed location variables to loc

--- client/html/View/MainView.js
+++ client/html/View/MainView.js
@@ -116,7 +116,7 @@ MainView.prototype._moveAnimation = function (nodes, translation_x, time, smooth
         initialPos[i] = this.Imports.DomHelper.getPos(nodes[i]).x;
     }
     setTimeout(function () {
-        doMove()
+        doMove();
     }, 1000 / this._MAXFPS - new Date().getTime() + initialTime);
 };
 
