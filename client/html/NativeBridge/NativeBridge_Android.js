b12eb95c8db821f28a95abcabb2789e22561aacf
Christopher Cook <cucook@gmail.com>
2013-03-18 22:59:20 -0700

    Refactored NativeBridge interface for symmetry

--- client/html/NativeBridge/NativeBridge_Android.js
+++ client/html/NativeBridge/NativeBridge_Android.js
@@ -9,36 +9,40 @@ function NativeBridge_Android() {
 
 NativeBridge_Android.prototype = Object.create(NativeBridge_Abstract.prototype);
 
-NativeBridge_Android.prototype._getLocationUpdates = function (activate, callbackID) {
-	AndroidInterface.getLocationUpdates(activate, callbackID);
+NativeBridge_Android.prototype._startLocationUpdates = function (callbackID) {
+	AndroidInterface.startLocationUpdates(callbackID);
 };
 
-NativeBridge_Android.prototype._getOrientationUpdates = function (activate, callbackID) {
-	AndroidInterface.getOrientationUpdates(activate, callbackID);
+NativeBridge_Android.prototype._stopLocationUpdates = function () {
+	AndroidInterface.stopLocationUpdates();
 };
 
-NativeBridge_Android.prototype._getCurrentLocation = function (callbackID) {
-	AndroidInterface.getCurrentLocation(callbackID);
+NativeBridge_Android.prototype._startOrientationUpdates = function (callbackID) {
+	AndroidInterface.startOrientationUpdates(callbackID);
 };
 
-NativeBridge_Android.prototype.showFireMissileScreen = function (activate) {
-	AndroidInterface.showFireMissileScreen(activate);
+NativeBridge_Android.prototype._stopOrientationUpdates = function () {
+	AndroidInterface.stopOrientationUpdates();
 };
 
-NativeBridge_Android.prototype._getPreference = function (preferences, callbackID) {
-	AndroidInterface.getPreference(preferences, callbackID);
+NativeBridge_Android.prototype.showFireMissileScreen = function () {
+	AndroidInterface.showFireMissileScreen();
 };
 
-NativeBridge_Android.prototype._setPreference = function (preferences, callbackID) {
-	AndroidInterface.setPreference(preferences, callbackID);
+NativeBridge_Android.prototype.hideFireMissileScreen = function () {
+	AndroidInterface.hideFireMissileScreen();
+};
+
+NativeBridge_Android.prototype._getPreferences = function (preferences, callbackID) {
+	AndroidInterface.getPreferences(preferences, callbackID);
+};
+
+NativeBridge_Android.prototype._setPreferences = function (preferences, callbackID) {
+	AndroidInterface.setPreferences(preferences, callbackID);
 };
 
 NativeBridge_Android.prototype._getFacebookAccessToken = function (callbackID) {
 	AndroidInterface.getFacebookAccessToken(callbackID);
-};
-
-NativeBridge_Android.prototype._onMainMenu = function (areWe) {
-	AndroidInterface.onMainMenu(areWe);
 };
 
 NativeBridge_Android.prototype.logoutFacebook = function () {
