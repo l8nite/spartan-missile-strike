9eab88a6e1c084305b0da3ce1323ced9d2d6a7c7
Jomana Sherif <techjomana@gmail.com>
2013-03-30 09:38:21 -0700

    Fixed naming, and Images

--- client/ios/SpartanMissileStrike/SSMainViewController.m
+++ client/ios/SpartanMissileStrike/SSMainViewController.m
@@ -2,7 +2,7 @@
 //  SSMainViewController.m
 //  SpartanMissileStrike
 //
-//  Created by Shaun Guth on 3/11/13.
+//  Created   on 3/11/13.
 //  Copyright (c) 2013 missileapp.com. All rights reserved.
 //
 
