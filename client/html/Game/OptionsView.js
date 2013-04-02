95b3e3bf505f0b491d725a605f42f0bb7c5436f3
Christopher Cook <cucook@gmail.com>
2013-04-02 15:33:02 -0700

    Fixed initial Options bugs and cleaned as per naming conventions

--- client/html/Game/OptionsView.js
+++ client/html/Game/OptionsView.js
@@ -1,19 +1,10 @@
 /* MissileApp options view.
- * 
- * Must copy-paste the OpponentsView.html markup into the app.
  */
 function OptionsView(Imports) {
 	var that = this;
 	this.Imports = Imports;
 	View.call(this, Imports.domId["OptionsView"]);
 
-	$("#" + Imports.domId["Options"] + " .startBtn").click(function () {
-		var opponentid = $("#options-input").val();
-		if (!Imports.Views["BaseView"]) {
-			Imports.Views["BaseView"] = new BaseView(Imports);
-		}
-		Imports.Views["BaseView"].show(optionsid);
-	});
 	$("#" + Imports.domId["OptionsView"] + " .backBtn").click(function () {
 		Imports.ViewManager.previousView();
 	});
