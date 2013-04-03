a40e0c056f8d23f911b6c334c567f7a6ffff56ef
Jomana Sherif <techjomana@gmail.com>
2013-04-03 11:41:34 -0700

    I am adding the images and thinking of using jQuerry

--- client/html/Game/OptionsView.js
+++ client/html/Game/OptionsView.js
@@ -1,11 +1,27 @@
 /* MissileApp options view.
  */
+ 
+ 
+
+ jQuery(function(){
+    jQuery('.MusicOn').live("click", function(e){
+        var src = $(this).attr("src");
+        $(this).attr("src", $(this).data("altsrc"));
+        $(this).data("altsrc", src);
+    });
+});
+
+
 function OptionsView(Imports) {
 	var that = this;
 	this.Imports = Imports;
 	View.call(this, Imports.domId["OptionsView"]);
 
 	$("#" + Imports.domId["OptionsView"] + " .backBtn").click(function () {
+		Imports.ViewManager.previousView();
+	});
+	
+		$("#" + Imports.domId["OptionsView"] + " .sfxToggle").click(function () {
 		Imports.ViewManager.previousView();
 	});
 }
@@ -24,21 +40,22 @@ OptionsView.prototype.show = function () {
 	this.Imports.ViewManager.loadView(this);
 };
 
-<script type="text/javascript">
-function func()
-{
-var afterImageClick = document.createElement("afterClick");
-afterImageClick.src = "./images/musicOn.png";
 
-document.getElementById('musicToggle').onclick = function() {
-       if (document.getElementById('yes').checked) {
-           alert('music on');
-       } else  (document.getElementById('no').checked) {
-           alert('music on');
-       } 
-       }
-
-}
-</script>
+ 
+// function func()
+// {
+// var afterImageClick = document.createElement("afterClick");
+// afterImageClick.src = "./images/musicOn.png";
+// 
+// document.getElementById('musicToggle').onclick = function() {
+//        if (document.getElementById('yes').checked) {
+//            alert('music on');
+//        } else  (document.getElementById('no').checked) {
+//            alert('music on');
+//        } 
+//        }
+// 
+// }
+//  
 
 
