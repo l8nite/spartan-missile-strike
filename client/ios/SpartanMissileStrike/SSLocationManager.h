c2c5639bfa24182c3b9f71736ca6eab4f4299e31
Jomana Sherif <techjomana@gmail.com>
2013-04-05 14:25:09 -0700

    SSMainViewController.m is not accepting the two lines

--- client/ios/SpartanMissileStrike/SSLocationManager.h
+++ client/ios/SpartanMissileStrike/SSLocationManager.h
@@ -12,9 +12,11 @@
 typedef void (^SSLocationManagerLocationChangedCallback)(CLLocationCoordinate2D location);
 
 @interface SSLocationManager : NSObject <CLLocationManagerDelegate>
-@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLLocationManager *locationManager ;
+@property (nonatomic, retain) CLLocationManager *azimuth;
 @property (strong, nonatomic) SSLocationManagerLocationChangedCallback locationManagerCallback;
 
 -(void)startUpdatingLocationWithCallback:(SSLocationManagerLocationChangedCallback)callback;
 -(void)stopUpdatingLocation;
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading; 
 @end
