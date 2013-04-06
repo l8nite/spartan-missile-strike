b4ac9f3679edeb14f5b94e7875a10e826e71a38c
Jomana Sherif <techjomana@gmail.com>
2013-04-05 17:59:52 -0700

    was workig on the location firgured it out and worked on the orientation referenced a stack overflow that explained azimuth and altitude and what they are according to pitch yaw and roll please test on your device and give me the ok to merge

--- client/ios/SpartanMissileStrike/SSLocationManager.h
+++ client/ios/SpartanMissileStrike/SSLocationManager.h
@@ -13,10 +13,8 @@ typedef void (^SSLocationManagerLocationChangedCallback)(CLLocationCoordinate2D 
 
 @interface SSLocationManager : NSObject <CLLocationManagerDelegate>
 @property (strong, nonatomic) CLLocationManager *locationManager ;
-@property (nonatomic, retain) CLLocationManager *azimuth;
 @property (strong, nonatomic) SSLocationManagerLocationChangedCallback locationManagerCallback;
 
 -(void)startUpdatingLocationWithCallback:(SSLocationManagerLocationChangedCallback)callback;
 -(void)stopUpdatingLocation;
-- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading; 
 @end
