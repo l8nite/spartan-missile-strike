782da158ea5eff7bbc102d68052c1ed8f9755d18
Christopher Cook <cucook@gmail.com>
2013-04-13 18:16:01 -0700

    Removed useless comment

--- client/html/Game/PlayToStart.js
+++ client/html/Game/PlayToStart.js
@@ -56,7 +56,6 @@ function PlayToStart(Imports) {
 					var sessionid = response.session.id,
 						userid = response.user.id,
 						isNewUser = response.user.newUser;
-					// Now we make the GM and start the game!
 					Imports.GameMaster = new GameMaster(userid, sessionid, Imports);
 					Imports.Views["MainMenu"] = new MainMenu(Imports);
 					Imports.Views["MainMenu"].show();
