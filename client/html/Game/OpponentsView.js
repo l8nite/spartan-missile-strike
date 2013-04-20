1ccf5a764cc9be7a116b436a784a6e16e75d1e4b
Christopher Cook <cucook@gmail.com>
2013-04-20 14:57:55 -0700

    List of opponents rendered

--- client/html/Game/OpponentsView.js
+++ client/html/Game/OpponentsView.js
@@ -5,13 +5,10 @@ function OpponentsView(Imports) {
 	this.Imports = Imports;
 	View.call(this, Imports.domId["OpponentsView"]);
 
-	$("#" + Imports.domId["OpponentsView"] + " .startBtn").click(function () {
-		var opponentid = $("#opponent-input").val();
-		if (!Imports.Views["BaseView"]) {
-			Imports.Views["BaseView"] = new BaseView(Imports);
-		}
-		Imports.Views["BaseView"].show(opponentid);
-	});
+	$("#" + that.Imports.domId["OpponentsView"])
+	.css("background-image", "url(\"../shared/Image Assets/spartanStrike_BG.png\")")
+	.css("background-size", "100%");
+
 	$("#" + Imports.domId["OpponentsView"] + " .backBtn").click(function () {
 		Imports.ViewManager.previousView();
 	});
@@ -20,6 +17,24 @@ function OpponentsView(Imports) {
 OpponentsView.prototype = Object.create(View.prototype);
 
 OpponentsView.prototype.onView = function () {
+	var that = this;
+	this.Imports.GameMaster.getOpponentsFromService().done(function (opponents) {
+		$("#" + that.Imports.domId["OpponentsView"] + " .player-list").empty();
+		for (var i = 0; i < opponents.length; i++) {
+			(function () {
+				var opponent = opponents[i];
+				$("#" + that.Imports.domId["OpponentsView"] + " .player-list").append(
+					$("<div>" + opponent.username + "</div>")
+					.click(function () {
+						if (!that.Imports.Views["BaseView"]) {
+							that.Imports.Views["BaseView"] = new BaseView(that.Imports);
+						}
+						that.Imports.Views["BaseView"].show(opponent.id);
+					})
+				);
+			})();
+		}
+	});
 	View.prototype.onView.call(this);
 };
 
