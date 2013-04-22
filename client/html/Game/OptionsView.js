f5f6f1a718994fb5d839e868af397dfd39cca6b8
Shaun Guth <shaun.guth@gmail.com>
2013-04-21 19:30:04 -0700

    hand-rolled positioning code, fixes #172

--- client/html/Game/OptionsView.js
+++ client/html/Game/OptionsView.js
@@ -57,15 +57,79 @@ function OptionsView(Imports) {
                 Imports.NativeBridge.stopSound(); // no param = stop all sounds
             }
         });
+
+        // BEWARE: thar be dragons here
+        // 62% of screen width, 3:2 aspect ratio 
+        var size = {};
+        size.width = $(window).width() * 0.62;
+        size.height = (size.width * 2) / 3;
+
+        // horizontally centered, 15% from top of screen
+        var position = {
+            left: ($(window).width() - size.width) / 2,
+            top: $(window).height() * 0.15
+        };
+
+        var optionsImage = $('#options-view #options-image');
+        var musicButton = $('#options-view #musicButton');
+        var sfxButton = $('#options-view #sfxButton');
+        var musicX = $('#options-view #musicX');
+        var sfxX = $('#options-view #sfxX');
+
+        $(optionsImage).css({
+            'width' : size.width,
+            'height' : size.height,
+            'left': position.left,
+            'top': position.top,
+            'position': 'absolute',
+        });
+
+        $(musicButton).css({
+            'width': size.width / 2,
+            'height': size.height,
+            'left': position.left,
+            'top': position.top,
+            'position': 'absolute',
+        });
+
+        $(sfxButton).css({
+            'width': size.width / 2,
+            'height': size.height,
+            'left': position.left + size.width / 2,
+            'top': position.top,
+            'position': 'absolute',
+        });
+
+        // "X" is 21% and 31% of width/height, positions are custom offsets because the options image isn't uniformly centered
+        var xSize = {};
+        xSize.width = (size.width) * 0.21;
+        xSize.height = (size.height) * 0.31;
+
+        $(musicX).css({
+            'width': xSize.width,
+            'height': xSize.height,
+            'left': $(musicButton).position().left + ($(musicButton).width() * 0.59 - xSize.width / 2),
+            'top': $(musicButton).position().top + ($(musicButton).height() * 0.56 - xSize.height / 2),
+            'position': 'absolute',
+        });
+
+        $('#options-view #sfxX').css({
+            'width': xSize.width,
+            'height': xSize.height,
+            'left': $(sfxButton).position().left + ($(sfxButton).width() * 0.40 - xSize.width / 2),
+            'top': $(sfxButton).position().top + ($(sfxButton).height() * 0.56 - xSize.height / 2),
+            'position': 'absolute',
+        });
+
+        // install handler to dismiss the options view if the user clicks outside of the box
+        $('#options-view #clickToDismiss').click(function(event) {
+            event.stopPropagation();
+            Imports.ViewManager.previousView();
+        });
     });
 
     this.Imports = Imports;
     View.call(this, Imports.domId["OptionsView"]);
-
-    $('#optionsView > #clickToDismiss').click(function(event) {
-        event.stopPropagation();
-        Imports.ViewManager.previousView();
-    });
 }
 
 OptionsView.prototype = Object.create(View.prototype);
